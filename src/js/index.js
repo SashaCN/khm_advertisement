f9995d73c6b370955a10ba11071c49b7ef64aa94
CN <sashacn228@gmail.com>
2023-06-24 11:48:07 +0300

    creating screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+/* -------------------------------------------------------------------------- */
+/*                              SERVICES ACORDION                             */
+/* -------------------------------------------------------------------------- */
+
 let services = document.querySelectorAll('.service');
 
 console.log(services)
@@ -10,6 +14,25 @@ services.forEach(service =>
     }
     if(current != service){
       service.classList.add('showed-desc');
+    }
+  })
+);
+
+/* -------------------------------------------------------------------------- */
+/*                                CREATING LIST                               */
+/* -------------------------------------------------------------------------- */
+
+let items = document.querySelectorAll('.adv-examples a');
+
+items.forEach(item =>
+  item.addEventListener("click", function(e) {
+    e.preventDefault();
+    let current = document.querySelector('.adv-examples a.choosen-item');
+    if(current != undefined) {
+      current.classList.remove('choosen-item');
+    }
+    if(current != item){
+      item.classList.add('choosen-item');
     }
   })
 );
