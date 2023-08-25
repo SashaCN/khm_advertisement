5684ef54723d4c40d50956d5e72d6375b88aa4ff
CN <sashacn228@gmail.com>
2023-08-25 13:09:42 +0300

    our works on main page change

--- src/php/gallery.php
+++ src/php/gallery.php
@@ -4,16 +4,23 @@ $directory = 'img/pages'; // Direction of images
 
 // Get all images
 $files = scandir($directory);
+// $images = [];
 
 foreach ($files as $file) {
-    // Check the extension of the image
     $extension = pathinfo($file, PATHINFO_EXTENSION);
     $allowed_extensions = array('jpg', 'jpeg', 'png', 'gif');
     
     if (in_array(strtolower($extension), $allowed_extensions)) {
+        // $imageInfo = exif_read_data($directory . '/' . $file);
+        // $creationDate = $imageInfo['FileDateTime'];
+        // $images[] = [$directory . '/' . $file, $creationDate];
+
         // Show image
-        echo '<figure><img src="' . $directory . '/' . $file . '" alt="' . $file . '" /></figure>';
+        echo '<figure><img src="' . $directory . '/' . $file . '" alt="' . $file . '" loading="lazy" /></figure>';
     }
 }
+// echo "<pre>";
+// var_dump($images);
+// echo "</pre>";
 
 ?>
